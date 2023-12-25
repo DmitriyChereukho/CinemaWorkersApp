fab659a2f5e78c9a8e72fbfd410b86e18d716cfc
Dmitriy Chereukho <dachereukho@edu.hse.ru>
2023-12-26 00:14:45 +0300

    HOT FIX!!!!

--- src/main/kotlin/domain/LoginController.kt
+++ src/main/kotlin/domain/LoginController.kt
@@ -2,6 +2,7 @@ package domain
 
 import data.LoginDao
 import presentation.models.LoginOutputModel
+import java.security.MessageDigest
 
 
 interface LoginController {
@@ -15,7 +16,7 @@ class LoginControllerImpl(private val loginDao: LoginDao) : LoginController {
         if (loginDao.containsLogin(login)) {
             return LoginOutputModel("Пользователь с таким логином уже существует", false)
         }
-        loginDao.add(login, password)
+        loginDao.add(login, hashPassword(password))
         return LoginOutputModel("Регистрация завершена", true)
     }
 
@@ -23,9 +24,24 @@ class LoginControllerImpl(private val loginDao: LoginDao) : LoginController {
         if (!loginDao.containsLogin(login)) {
             return LoginOutputModel("Пользователя с таким логином не существует", false)
         }
-        if(!loginDao.check(login, password)){
+        if (!loginDao.check(login, hashPassword(password))) {
             return LoginOutputModel("Пароль неверный", false)
         }
         return LoginOutputModel("Авторизация завершена", true)
+    }
+
+    private fun hashPassword(password: String): String {
+        val md = MessageDigest.getInstance("SHA-256")
+        val bytes = md.digest(password.toByteArray(Charsets.UTF_8))
+        return bytesToString(bytes)
+    }
+
+
+    private fun bytesToString(bytes: ByteArray): String {
+        val stringBuilder = StringBuilder()
+        for (byte in bytes) {
+            stringBuilder.append(String.format("%02x", byte))
+        }
+        return stringBuilder.toString()
     }
 }
